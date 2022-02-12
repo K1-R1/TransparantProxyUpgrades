cd9f28f3a9a541d0ee4e9591862f05d151cf3cfe
KR <kyranrawlinson@gmail.com>
2022-02-12 23:06:46 +0000

    Updated scripts/general_scripts.py
    Updated with function; upgrade, to upgrade a proxy with a new implementation

--- scripts/general_scripts.py
+++ scripts/general_scripts.py
@@ -16,4 +16,24 @@ def encode_function_data(initialiser=None, *args):
         return eth_utils.to_bytes(hexstr='0x')
     
     return initialiser.encode_input(*args)
+
+def upgrade(account, proxy, new_implemtation, proxy_admin=None, initialiser=None, *args):
+    if proxy_admin:
+        if initialiser:
+            encoded_function_call = encode_function_data(initialiser, *args)
+            tx = proxy_admin.upgradeAndCall(proxy.address, new_implemtation.address, encoded_function_call, {'from':account})
+            tx.wait(1)
+        else:
+            tx = proxy_admin.upgrade(proxy.address, new_implemtation.address, {'from':account})
+            tx.wait(1)
     
+    elif initialiser:
+        encoded_function_call = encode_function_data(initialiser, *args)
+        tx = proxy.upgradeToAndCall(new_implemtation.address, encoded_function_call, {'from':account})
+        tx.wait(1)
+
+    else:
+        tx = proxy.upgradeTo(new_implemtation.address, {'from':account})
+        tx.wait(1)
+
+    return tx
